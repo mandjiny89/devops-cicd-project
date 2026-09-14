03f0be520e6bb19a593de094892fca979f59415e
Pat Mandjiny <patmandjiny@Pats-MacBook-Pro.local>
2026-09-14 11:32:40 +0100

    Add full CI/CD pipeline with Terraform, Docker, ECR and Ansible

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -9,9 +9,9 @@ resource "aws_ecr_lifecycle_policy" "app" {
   policy = jsonencode({
     rules = [{
       rulePriority = 1
-      description = "Keep latest 10 images"
-      selection = { tagStatus="any", countType="imageCountMoreThan", countNumber=10 }
-      action = { type="expire" }
+      description  = "Keep latest 10 images"
+      selection    = { tagStatus = "any", countType = "imageCountMoreThan", countNumber = 10 }
+      action       = { type = "expire" }
     }]
   })
 }
